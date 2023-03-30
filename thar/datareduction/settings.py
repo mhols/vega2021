97f8778e961c56183059987744eeb0681720dc30
Matthias Holschneider <matthias.holschneider@uni-potsdam.de>
2023-03-30 10:14:58 +0200

    one step

--- thar/datareduction/settings.py
+++ thar/datareduction/settings.py
@@ -119,7 +119,7 @@ CENTRALPOSITION = {  ### TODO: move to reffiles...
     # 59:3302}  TODO make robust for empty beams
 
 ORDERS = list(CENTRALPOSITION.keys())
-LAMBDAFILE = os.path.join(REFFILES, 'hobo.txt')
+LAMBDAFILE = os.path.join(REFFILES, 'artlambda2254correct.dat')
 #os.environ.get('LAMBDAFILE', 
 #            os.path.join(REFFILES, 'hobo.txt'))  # TODO to be removed....
 OFFSET_LAMBDA = 0
