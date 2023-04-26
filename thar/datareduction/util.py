2c76d5f2ba77b83ed71265028baa1d6a0b42e3d0
Matthias Holschneider <matthias.holschneider@uni-potsdam.de>
2023-04-27 00:00:03 +0200

    progressing

--- thar/datareduction/util.py
+++ thar/datareduction/util.py
@@ -23,7 +23,7 @@ def loss_1(params, *args):
     intens, func = args
     n = np.arange(intens.shape[0])
     i = func(n, *params)
-    return np.abs(i-intens)/np.sqrt(np.abs(i)+1e-8)
+    return np.abs(i-intens)*np.sqrt(np.abs(i))
     
 def air_to_vac(lam):
 	#lambda_vac (see https://www.astro.uu.se/valdwiki/Air-to-vacuum%20conversion)
@@ -50,7 +50,7 @@ def loss_3(params, *args):
     intens, func = args
     n = np.arange(intens.shape[0])
     i = func(n, *params)
-    return (i-intens)/np.sqrt(np.where(intens>1, intens, 1))
+    return (i-intens)*np.sqrt(np.where(intens>1, intens, 1))
 
 
 function_map = {
