0c42bc1f87304547329a714f266d609a8665faf7
Matthias Holschneider <matthias.holschneider@uni-potsdam.de>
2023-06-19 16:25:36 +0200

    progressing

--- thar/datareduction/extract.py
+++ thar/datareduction/extract.py
@@ -869,9 +869,84 @@ class Extractor_level_1:
                 * self.blaze(o)(self.CENTRALROW)
                 / self.beams[o].beam_sum_voie2(self.masterflat) for o in self.ORDERS
             }
+        elif choice == 'OPTIMAL_EXTRACT':
+            self._voie1 = self._voie_optimal(1)
+            self._voie2 = self._voie_optimal(2)
         else:
             raise Exception('no such method ' + choice)
         return
+
+    def _voie_optimal(self, voie):
+        """
+        optimal extraction
+        """
+
+        ADU_FACTOR = 3.
+        READOUT_NOISE = 1.
+        COSMIC_SIGMA_CLIP = 3
+        COSMIC_MIN_PIXEL = 8
+
+        m = self.kwargs['SHIFT_MASK_VOIE1'] if voie == 1 else self.kwargs['SHIFT_MASK_VOIE2']
+        n = len(m)
+
+        res = {}
+        for o in self.ORDERS:
+            E = self.beams[o].mask_voie1 if voie==1 else self.beams[o].mask_voie2  # extraction from image
+            N = np.count_nonzero(E)  # number of points in mask
+
+            F = self.masterflat[E].reshape((N//n, n))
+            v = self.image[E].reshape((N//n, n))
+
+            F = np.where(F>0, F, 0)
+            v = np.where(v>0, v, 0)
+
+            sig2 = READOUT_NOISE + ADU_FACTOR * v
+            M = np.ones(F.shape)  # selection mask of pixels without cosmics..
+
+            # print('\n')
+            for i in range(10):      # maximal number of iterations 
+                # print (o, i, np.count_nonzero(M))
+
+                # number of extracted spectral values
+                nd = np.count_nonzero(np.sum(M, axis=1))
+                nu = np.count_nonzero(M)   # number of unmasked pixels.
+
+                w = M / sig2  # weight including pixel mask
+                G = np.any(M>0, axis=1) # boolean mask on pixel lines (True if line not empty)
+
+                # lsq estimate of lam. On empty lines put nan.
+                lam = np.where( G, np.sum(w * F * v, axis=1) / np.sum(w * F**2, axis=1), np.nan)
+
+                vv = lam[:, None] * F  # estimated/predicted pixel pattern under model assumption
+
+                # esimating reduced chi2 sum in order to estimate global noise level
+                chi2red = np.sum(w * (v - vv)**2) / (nu-nd)
+                sig2_sf = chi2red * sig2   # rescaled noise level from data
+
+
+                post_sig2 = np.where( w>0, 
+                    sig2_sf * \
+                    (1 - w * F**2 / (np.sum(w * F**2, axis=1))[:, None]),
+                    np.nan
+                )
+                sig2 = READOUT_NOISE + ADU_FACTOR * vv  # basing noise structure on estimated average
+                
+
+                # one sided test ? or better two sided outlier removal ?
+                MM = np.where((v-vv) <= COSMIC_SIGMA_CLIP * np.sqrt(post_sig2), 1, 0)
+
+                # detetect lines with many bad pixels
+                I = np.count_nonzero(MM, axis=1)
+                J, = np.where(I <= COSMIC_MIN_PIXEL)
+                MM[J,:] = 0    # elimination of whole row if not enough pixels good
+
+                if (np.all(MM==M)):   # repeat until selected set does not change anymore
+                    break
+                M = MM
+
+            res[o] = lam
+
+        return res
 
     @property
     def voie1(self):
@@ -1459,77 +1534,8 @@ class Extractor(PlotExtractMixin, Extractor_level_2):
                        get_ext({self.fitsfile}')
         """
 
-        def voie1_optimal(self):
-        """
-        optimal extraction
-        """
-
-        ADU_FACTOR = 3.
-        READOUT_NOISE = 1.
-        COSMIC_SIGMA_CLIP = 3
-        COSMIC_MIN_PIXEL = 8
-
-        m = self.kwargs['SHIFT_MASK_VOIE1']
-        n = len(m)
-
-        res = {}
-        for o in self.ORDERS:
-            E = self.beams[o].mask_voie1   # extraction from image
-            N = np.count_nonzero(E)  # number of points in mask
-
-            F = self.masterflat[E].reshape((N//n, n))
-            v = self.image[E].reshape((N//n, n))
-
-            F = np.where(F>0, F, 0)
-            v = np.where(v>0, v, 0)
-
-            sig2 = READOUT_NOISE + ADU_FACTOR * v
-            M = np.ones(F.shape)  # selection mask of pixels without cosmics..
-
-            print('\n')
-            for i in range(10):      # maximal number of iterations 
-                print (o, i, np.count_nonzero(M))
-
-                # number of extracted spectral values
-                nd = np.count_nonzero(np.sum(M, axis=1))
-                nu = np.count_nonzero(M)   # number of unmasked pixels.
-
-                w = M / sig2  # weight including pixel mask
-                G = np.any(M>0, axis=1) # boolean mask on pixel lines (True if line not empty)
-
-                # lsq estimate of lam. On empty lines put nan.
-                lam = np.where( G, np.sum(w * F * v, axis=1) / np.sum(w * F**2, axis=1), np.nan)
-
-                vv = lam[:, None] * F  # estimated/predicted pixel pattern under model assumption
-
-                # esimating reduced chi2 sum in order to estimate global noise level
-                chi2red = np.sum(w * (v - vv)**2) / (nu-nd)
-                sig2_sf = chi2red * sig2   # rescaled noise level from data
-
-
-                post_sig2 = np.where( w>0, 
-                    sig2_sf * \
-                    (1 - w * F**2 / (np.sum(w * F**2, axis=1))[:, None]),
-                    np.nan
-                )
-                sig2 = READOUT_NOISE + ADU_FACTOR * vv  # basing noise structure on estimated average
-                
-
-                # one sided test ? or better two sided outlier removal ?
-                MM = np.where((v-vv) <= COSMIC_SIGMA_CLIP * np.sqrt(post_sig2), 1, 0)
-
-                # detetect lines with many bad pixels
-                I = np.count_nonzero(MM, axis=1)
-                J, = np.where(I <= COSMIC_MIN_PIXEL)
-                MM[J,:] = 0    # elimination of whole row if not enough pixels good
-
-                if (np.all(MM==M)):   # repeat until selected set does not change anymore
-                    break
-                M = MM
-
-            res[o] = lam
-
-        return res
+   
+    
 
     @lazyproperty
     def snippets_voie1(self):
